8566cabff51aa5fce7e4c4450fe3f3c0c360a1f7
diego3893 <diegozcx@foxmail.com>
2025-12-11 10:33:17 +0800

    新增宏定义

--- src/rule/rule.h
+++ src/rule/rule.h
@@ -1,4 +1,18 @@
 #ifndef RULE_H
 #define RULE_H
 
+typedef struct{
+    int dx;
+    int dy;
+}DeltaPair;
+
+#define DELTA_RIGHT ((DeltaPair){1, 0})   
+#define DELTA_DOWN ((DeltaPair){0, -1})  
+#define DELTA_LEFT ((DeltaPair){-1, 0}) 
+#define DELTA_UP ((DeltaPair){0, 1}) 
+#define DELTA_UPRIGHT ((DeltaPair){1, 1}) 
+#define DELTA_UPLEFT ((DeltaPair){-1, 1}) 
+#define DELTA_DOWNLEFT ((DeltaPair){-1, -1})
+#define DELTA_DOWNRIGHT ((DeltaPair){1, -1})
+
 #endif 
