7980e86b8abca6945dc2059826dc75755b10f788
diego3893 <diegozcx@foxmail.com>
2025-12-11 10:20:06 +0800

    删除注释

--- src/board/board.c
+++ src/board/board.c
@@ -1,20 +1,5 @@
-/**
- * @file board.c
- * @author diego3893 (diegozcx@foxmail.com)
- * @brief 棋盘相关函数具体实现
- * @version 1.0
- * @date 2025-12-10
- * 
- * @copyright Copyright (c) 2025
- * 
- */
 #include"board.h"
 
-/**
- * @brief 初始化棋盘，将所有位置初始化为空，最新落子为-1（未落子），总落子数为0
- * 
- * @param board 棋盘
- */
 void boardInit(Board* board){
     if(board == NULL){
         return;
